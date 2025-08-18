5ccd43f01103fe6440fd5eb6491aefcb2eb21aac
Adam-Doudeau <adam.doudeau1@gmail.com>
2025-08-18 14:32:56 +0200

    Refactor product click event handling in search.js
    Replaces individual event listeners on each product thumbnail with a single delegated event listener on the products container. Improves page parameter handling for position calculation and streamlines the ajaxProductClick function.

--- views/js/front/search.js
+++ views/js/front/search.js
@@ -1,15 +1,9 @@
-listProducts = document.querySelectorAll('.thumbnail-container');
-
-listProducts.forEach((element, index) => {
-    element.addEventListener('click', function(event) {
-        ajaxProductClick(this.parentElement, index);
-    });
-});
-
 function ajaxProductClick(product, index) {
-    console.log(page); 
-    let position = 1 + index + 48*(parseInt(page, 10) - 1);
-    console.log(position);
+    let params = new URLSearchParams(document.location.search);
+    if( !params.has('page') ) {
+        params.set('page', 1);
+    }
+    let position = 1 + index + 48*(parseInt(params.get('page'), 10) - 1);
     const xhttp = new XMLHttpRequest();
     $url = base_url + "?token=1&action=productClick&id_product=" + product.dataset.idProduct + "&position=" + position;
     xhttp.onload = function() {
@@ -24,4 +18,15 @@ function ajaxProductClick(product, index) {
     xhttp.open("GET", $url, true);
     xhttp.getResponseHeader("Content-type", "text/html; charset=utf-8");
     xhttp.send();
+
 }
+
+let listProducts = document.getElementById('products');
+listProducts.addEventListener('click', function(event) {
+    if (event.target && event.target.closest('.thumbnail-container')) {
+        let product = event.target.closest('.product').firstElementChild;
+        let productsRow = document.querySelector('#js-product-list');
+        
+        ajaxProductClick(product, Array.from(productsRow.firstElementChild.children).indexOf(product.parentElement));
+    }
+});
