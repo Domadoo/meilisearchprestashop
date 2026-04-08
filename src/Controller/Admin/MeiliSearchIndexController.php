65dfbae49554b8f3bc5ddeb299ad15959c49b5e3
Adam-Doudeau <adam.doudeau1@gmail.com>
2026-04-08 11:10:04 +0200

    Add index settings and reindexing features
    Add routes and UI for index settings and reindexing, and refactor product indexing.
    
    - Added routes: index settings, single index reindex, and bulk reindex.
    - New MeiliSearchIndexSettingsController to display index details and settings.
    - Refactored MeiliSearchIndexController: introduce private indexLanguage() used by indexProductsAction, reindexLanguageAction and bulkReindexAction; filter indexes by configured prefix; pass languages to the index template.
    - Updated admin index template: add language reindex form, bulk reindex option, per-index reindex button, CSRF tokens, and JS handlers for reindex actions.
    - Added new index settings Twig template (placeholder page).
    
    These changes add server endpoints and UI to reindex one or multiple language-specific product indexes, show index settings, and ensure only indexes matching the configured prefix are listed.

--- src/Controller/Admin/MeiliSearchIndexController.php
+++ src/Controller/Admin/MeiliSearchIndexController.php
@@ -44,6 +44,7 @@ class MeiliSearchIndexController extends FrameworkBundleAdminController
     public function indexAction()
     {
         $meiliUrl = Configuration::get('MEILISEARCHPRESTASHOP_URL');
+        $meiliPrefix = Configuration::get('MEILISEARCHPRESTASHOP_PREFIX');
         $indexes = [];
 
         if ($meiliUrl) {
@@ -53,6 +54,9 @@ class MeiliSearchIndexController extends FrameworkBundleAdminController
 
             if ($response && isset($response->results)) {
                 foreach ($response->results as $index) {
+                    if ($meiliPrefix && strpos($index->uid, $meiliPrefix) !== 0) {
+                        continue;
+                    }
                     $index->numberOfDocuments = isset($indexStats[$index->uid]) ? $indexStats[$index->uid]->numberOfDocuments : null;
                     $indexes[] = $index;
                 }
@@ -61,7 +65,10 @@ class MeiliSearchIndexController extends FrameworkBundleAdminController
 
         return $this->render('@Modules/meilisearchprestashop/views/templates/admin/index.html.twig', array_merge(
             $this->getTranslatedText(),
-            ['indexes' => $indexes]
+            [
+                'indexes' => $indexes,
+                'languages' => Language::getLanguages(),
+            ]
         ));
     }
 
@@ -121,142 +128,189 @@ class MeiliSearchIndexController extends FrameworkBundleAdminController
 
     public function indexProductsAction()
     {
-        $languages = Language::getLanguages();
-
-        foreach ($languages as $language) {
-            $id_lang = $language['id_lang'];
-            $iso_code = $language['iso_code'];
-            $meiliUrl = Configuration::get('MEILISEARCHPRESTASHOP_URL');
-
-
-            $sql = '
-                SELECT p.*, product_shop.*, pl.*,
-                    m.`name` AS manufacturer_name,
-                    s.`name` AS supplier_name
-                FROM `' . _DB_PREFIX_ . 'product` p
-                ' . Shop::addSqlAssociation('product', 'p') . '
-                LEFT JOIN `' . _DB_PREFIX_ . 'product_lang` pl
-                    ON (p.`id_product` = pl.`id_product` ' . Shop::addSqlRestrictionOnLang('pl') . ')
-                LEFT JOIN `' . _DB_PREFIX_ . 'manufacturer` m
-                    ON (m.`id_manufacturer` = p.`id_manufacturer`)
-                LEFT JOIN `' . _DB_PREFIX_ . 'supplier` s
-                    ON (s.`id_supplier` = p.`id_supplier`)
-                WHERE pl.`id_lang` = ' . (int) $id_lang . '
-                AND product_shop.`active` = 1
-            ';
-
-            $products = Db::getInstance(true)->executeS($sql);
-
-
-            $typeMap = [
-                'id_product' => 'int',
-                'id_supplier' => 'int',
-                'id_manufacturer' => 'int',
-                'id_category_default' => 'int',
-                'id_shop_default' => 'int',
-                'id_tax_rules_group' => 'int',
-                'on_sale' => 'bool',
-                'online_only' => 'bool',
-                'low_stock_alert' => 'bool',
-                'quantity' => 'int',
-                'minimal_quantity' => 'int',
-                'price' => 'float',
-                'wholesale_price' => 'float',
-                'unit_price_ratio' => 'float',
-                'additional_shipping_cost' => 'float',
-                'width' => 'float',
-                'height' => 'float',
-                'depth' => 'float',
-                'weight' => 'float',
-                'out_of_stock' => 'int',
-                'additional_delivery_times' => 'bool',
-                'quantity_discount' => 'bool',
-                'customizable' => 'bool',
-                'uploadable_files' => 'bool',
-                'text_fields' => 'bool',
-                'active' => 'bool',
-                'id_type_redirected' => 'int',
-                'available_for_order' => 'bool',
-                'show_condition' => 'bool',
-                'show_price' => 'bool',
-                'indexed' => 'bool',
-                'cache_is_pack' => 'bool',
-                'cache_has_attachments' => 'bool',
-                'is_virtual' => 'bool',
-                'cache_default_attribute' => 'int',
-                'advanced_stock_management' => 'bool',
-                'pack_stock_type' => 'int',
-                'state' => 'int',
-                'atoosync' => 'bool',
-                'id_shop' => 'int',
-                'final_price' => 'float',
-                'id_lang' => 'int',
-            ];
-
-            $productIds = array_column($products, 'id_product');
-            $productIdsStr = implode(',', array_map('intval', $productIds));
-
-            $featuresSql = '
-                SELECT id_product, id_feature, id_feature_value
-                FROM `' . _DB_PREFIX_ . 'feature_product`
-                WHERE id_product IN (' . $productIdsStr . ')
-            ';
-
-            $featureResults = Db::getInstance()->executeS($featuresSql);
-
-            $productFeatureValues = [];
-            foreach ($featureResults as $row) {
-                $idProduct = (int) $row['id_product'];
-                $featureKey = (int) $row['id_feature'] . '-' . (int) $row['id_feature_value'];
-
-                if (!isset($productFeatureValues[$idProduct])) {
-                    $productFeatureValues[$idProduct] = [];
-                }
-
-                $productFeatureValues[$idProduct][] = $featureKey;
-            }
-
-            foreach ($products as &$product) {
-                foreach ($typeMap as $field => $type) {
-                    if (array_key_exists($field, $product) && $product[$field] !== null) {
-                        switch ($type) {
-                            case 'int':
-                                $product[$field] = (int) $product[$field];
-                                break;
-                            case 'float':
-                                $product[$field] = (float) $product[$field];
-                                break;
-                            case 'bool':
-                                $product[$field] = (bool) $product[$field];
-                                break;
-                        }
-                    }
-                }
-                $id = $product['id_product'];
-                $product['feature_values'] = $productFeatureValues[$id] ?? [];
-            }
-            unset($product);
-
-            $payloadIndex = json_encode([
-                'uid' => Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code,
-                'primaryKey' => 'id_product',
-            ]);
-            $this->module->requestCurl($meiliUrl . 'indexes', $payloadIndex);
-
-            $arrayProductsChunk = array_chunk($products, 200);
-
-            foreach ($arrayProductsChunk as $arrayProducts) {
-                $this->module->requestCurl($meiliUrl . 'indexes/' . Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code . '/documents', json_encode($arrayProducts));
-            }
-
-            $this->module->requestCurl($meiliUrl . 'indexes/' . Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code . '/settings/pagination', json_encode(['maxTotalHits' => 9999]), 'PATCH');
-            $this->module->requestCurl($meiliUrl . 'indexes/' . Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code . '/settings/sortable-attributes', json_encode(['name', 'price', 'date_add', 'quantity']), 'PUT');
-            $this->module->requestCurl($meiliUrl . 'indexes/' . Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code . '/settings/ranking-rules', json_encode(['sort', 'words', 'typo', 'proximity', 'attribute', 'exactness']), 'PUT');
-            $this->module->requestCurl($meiliUrl . 'indexes/' . Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code . '/settings/filterable-attributes', json_encode(['id_manufacturer', 'out_of_stock', 'condition', 'id_category_default', 'quantity', 'feature_values', 'visibility', 'available_for_order']), 'PUT');
+        foreach (Language::getLanguages() as $language) {
+            $this->indexLanguage($language);
         }
 
         $this->addFlash('success', 'Produits indexés avec succès dans Meilisearch.');
 
         return $this->redirectToRoute('admin_meilisearch_index_index');
+    }
+
+    public function reindexLanguageAction($uid)
+    {
+        $prefix = Configuration::get('MEILISEARCHPRESTASHOP_PREFIX');
+        $needle = $prefix . 'products_';
+
+        // uid peut être un nom d'index complet (shop1_products_fr) ou un iso_code seul (fr)
+        $iso_code = strpos($uid, $needle) === 0
+            ? substr($uid, strlen($needle))
+            : $uid;
+
+        foreach (Language::getLanguages() as $language) {
+            if ($language['iso_code'] === $iso_code) {
+                $this->indexLanguage($language);
+                $this->addFlash('success', sprintf('Index "%s" réindexé avec succès.', $uid));
+                return $this->redirectToRoute('admin_meilisearch_index_index');
+            }
+        }
+
+        $this->addFlash('error', sprintf('Impossible de réindexer l\'index "%s" : langue introuvable.', $uid));
+
+        return $this->redirectToRoute('admin_meilisearch_index_index');
+    }
+
+    public function bulkReindexAction(Request $request)
+    {
+        $uids = (array) $request->request->get('uids', []);
+        $prefix = Configuration::get('MEILISEARCHPRESTASHOP_PREFIX');
+        $needle = $prefix . 'products_';
+        $langByIso = array_column(Language::getLanguages(), null, 'iso_code');
+        $count = 0;
+
+        foreach ($uids as $uid) {
+            if (strpos($uid, $needle) === 0) {
+                $iso_code = substr($uid, strlen($needle));
+                if (isset($langByIso[$iso_code])) {
+                    $this->indexLanguage($langByIso[$iso_code]);
+                    $count++;
+                }
+            }
+        }
+
+        $this->addFlash('success', sprintf('%d index réindexé(s) avec succès.', $count));
+
+        return $this->redirectToRoute('admin_meilisearch_index_index');
+    }
+
+    private function indexLanguage(array $language)
+    {
+        $id_lang = $language['id_lang'];
+        $iso_code = $language['iso_code'];
+        $meiliUrl = Configuration::get('MEILISEARCHPRESTASHOP_URL');
+
+        $sql = '
+            SELECT p.*, product_shop.*, pl.*,
+                m.`name` AS manufacturer_name,
+                s.`name` AS supplier_name
+            FROM `' . _DB_PREFIX_ . 'product` p
+            ' . Shop::addSqlAssociation('product', 'p') . '
+            LEFT JOIN `' . _DB_PREFIX_ . 'product_lang` pl
+                ON (p.`id_product` = pl.`id_product` ' . Shop::addSqlRestrictionOnLang('pl') . ')
+            LEFT JOIN `' . _DB_PREFIX_ . 'manufacturer` m
+                ON (m.`id_manufacturer` = p.`id_manufacturer`)
+            LEFT JOIN `' . _DB_PREFIX_ . 'supplier` s
+                ON (s.`id_supplier` = p.`id_supplier`)
+            WHERE pl.`id_lang` = ' . (int) $id_lang . '
+            AND product_shop.`active` = 1
+        ';
+
+        $products = Db::getInstance(true)->executeS($sql);
+
+        $typeMap = [
+            'id_product' => 'int',
+            'id_supplier' => 'int',
+            'id_manufacturer' => 'int',
+            'id_category_default' => 'int',
+            'id_shop_default' => 'int',
+            'id_tax_rules_group' => 'int',
+            'on_sale' => 'bool',
+            'online_only' => 'bool',
+            'low_stock_alert' => 'bool',
+            'quantity' => 'int',
+            'minimal_quantity' => 'int',
+            'price' => 'float',
+            'wholesale_price' => 'float',
+            'unit_price_ratio' => 'float',
+            'additional_shipping_cost' => 'float',
+            'width' => 'float',
+            'height' => 'float',
+            'depth' => 'float',
+            'weight' => 'float',
+            'out_of_stock' => 'int',
+            'additional_delivery_times' => 'bool',
+            'quantity_discount' => 'bool',
+            'customizable' => 'bool',
+            'uploadable_files' => 'bool',
+            'text_fields' => 'bool',
+            'active' => 'bool',
+            'id_type_redirected' => 'int',
+            'available_for_order' => 'bool',
+            'show_condition' => 'bool',
+            'show_price' => 'bool',
+            'indexed' => 'bool',
+            'cache_is_pack' => 'bool',
+            'cache_has_attachments' => 'bool',
+            'is_virtual' => 'bool',
+            'cache_default_attribute' => 'int',
+            'advanced_stock_management' => 'bool',
+            'pack_stock_type' => 'int',
+            'state' => 'int',
+            'atoosync' => 'bool',
+            'id_shop' => 'int',
+            'final_price' => 'float',
+            'id_lang' => 'int',
+        ];
+
+        $productIds = array_column($products, 'id_product');
+        $productIdsStr = implode(',', array_map('intval', $productIds));
+
+        $featuresSql = '
+            SELECT id_product, id_feature, id_feature_value
+            FROM `' . _DB_PREFIX_ . 'feature_product`
+            WHERE id_product IN (' . $productIdsStr . ')
+        ';
+
+        $featureResults = Db::getInstance()->executeS($featuresSql);
+
+        $productFeatureValues = [];
+        foreach ($featureResults as $row) {
+            $idProduct = (int) $row['id_product'];
+            $featureKey = (int) $row['id_feature'] . '-' . (int) $row['id_feature_value'];
+
+            if (!isset($productFeatureValues[$idProduct])) {
+                $productFeatureValues[$idProduct] = [];
+            }
+
+            $productFeatureValues[$idProduct][] = $featureKey;
+        }
+
+        foreach ($products as &$product) {
+            foreach ($typeMap as $field => $type) {
+                if (array_key_exists($field, $product) && $product[$field] !== null) {
+                    switch ($type) {
+                        case 'int':
+                            $product[$field] = (int) $product[$field];
+                            break;
+                        case 'float':
+                            $product[$field] = (float) $product[$field];
+                            break;
+                        case 'bool':
+                            $product[$field] = (bool) $product[$field];
+                            break;
+                    }
+                }
+            }
+            $id = $product['id_product'];
+            $product['feature_values'] = $productFeatureValues[$id] ?? [];
+        }
+        unset($product);
+
+        $indexUid = Configuration::get('MEILISEARCHPRESTASHOP_PREFIX') . 'products_' . $iso_code;
+
+        $payloadIndex = json_encode([
+            'uid' => $indexUid,
+            'primaryKey' => 'id_product',
+        ]);
+        $this->module->requestCurl($meiliUrl . 'indexes', $payloadIndex);
+
+        foreach (array_chunk($products, 200) as $chunk) {
+            $this->module->requestCurl($meiliUrl . 'indexes/' . $indexUid . '/documents', json_encode($chunk));
+        }
+
+        $this->module->requestCurl($meiliUrl . 'indexes/' . $indexUid . '/settings/pagination', json_encode(['maxTotalHits' => 9999]), 'PATCH');
+        $this->module->requestCurl($meiliUrl . 'indexes/' . $indexUid . '/settings/sortable-attributes', json_encode(['name', 'price', 'date_add', 'quantity']), 'PUT');
+        $this->module->requestCurl($meiliUrl . 'indexes/' . $indexUid . '/settings/ranking-rules', json_encode(['sort', 'words', 'typo', 'proximity', 'attribute', 'exactness']), 'PUT');
+        $this->module->requestCurl($meiliUrl . 'indexes/' . $indexUid . '/settings/filterable-attributes', json_encode(['id_manufacturer', 'out_of_stock', 'condition', 'id_category_default', 'quantity', 'feature_values', 'visibility', 'available_for_order']), 'PUT');
     }
 }
